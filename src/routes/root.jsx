1f8cb666bb23b106f3f57e92e047cce3cd89d934
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-12 13:15:40 +0100

    first set of routes defined

---
+++ src/routes/root.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import Therapy from "../pages/Therapies";
+import Home from "../pages/Home";
+
+export const AppRoutes = () => {
+ return (
+    <Routes>
+        <Route path="/Therapies" element={<Therapy/>}/>
+        <Route path="" element= {<Home/>}/>
+    </Routes>
+ );   
+}
