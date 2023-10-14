4b4a18e53bb56e3d7ad8401aec3ec8561c69278d
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-14 09:20:54 +0100

    using the API response

--- src/pages/Therapies.jsx
+++ src/pages/Therapies.jsx
@@ -1,7 +1,20 @@
+import FetchTherapies from "../lib/fetchTherapies";
+import { useQuery } from "react-query";
+
 export default function Therapy () {
+    const {data} = useQuery({
+        queryKey: "therapies",
+        queryFn: async () => await FetchTherapies(),
+    });
+    console.log(data)
     return (
         <div>
             Therapies
+            {data.map((value)=> (
+                <div>
+                    {value?.data.data}
+                </div>
+                ))};
         </div>
     );
 }
