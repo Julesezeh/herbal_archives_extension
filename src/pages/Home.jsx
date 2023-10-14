d5f58efd21bdb8a06de7a6aa6be4bdf9292c6632
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-14 09:52:40 +0100

    root url styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
 export default function Home () {
     return (
-        <div>
+        <div style={homeStyle}>
             Hello
             
         </div>
     );
+}
+const homeStyle = {
+    marginTop:"30px",
+    fontSize:12,
+    color:"green"
 }
