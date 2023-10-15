9685e57547b7b916b408f7185490e12004be4345
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-15 01:44:04 +0100

    manigest file added to the build folder

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { viteStaticCopy } from "vite-plugin-static-copy"
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // viteStaticCopy({
-  //   targets: [
-  //     { src:{"mainfest.json",
-  //       dest: "" },
-  //   ]
-  // }),
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: "manifest.json", dest: "" },
+        { src: "src/assets/react.svg", dest: "" }
+      ],
+    })
+
+
+  ],
 })
