d08b162c89e2bb92b4743bd822e9289971458432
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-18 08:15:36 +0100

    Users route added

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Therapy from "../pages/Therapies";
 import Home from "../pages/Home";
+import Users from "../pages/Users";
 
 export const AppRoutes = () => {
  return (
     <Routes>
+        <Route path="/Users" element={<Users/>}/>
         <Route path="/Therapies" element={<Therapy/>}/>
         <Route path="*" element= {<Home/>}/>
     </Routes>
