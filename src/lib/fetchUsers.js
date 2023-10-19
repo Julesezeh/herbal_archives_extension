057b6037a0c7292c6f45dbe14967668732b1c5bb
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-19 08:23:54 +0100

    comment

--- src/lib/fetchUsers.js
+++ src/lib/fetchUsers.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-
+//not working
 export default async function FetchUsers() {
     return (
         await axios.get(`https://local-therapy-archives.onrender.com/api/v1/users`)
