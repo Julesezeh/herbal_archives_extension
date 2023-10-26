3d8b9645a75844e16175a2f8532af4590190062a
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-26 11:02:52 +0100

    routing set up for details

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Therapy from "../pages/Therapies/Therapies";
 import Home from "../pages/Home";
 import Users from "../pages/Users";
+import TherapyDetail from "../pages/Therapies/therapyDetail";
 
 export const AppRoutes = () => {
  return (
@@ -9,6 +10,7 @@ export const AppRoutes = () => {
         <Route path="/Users" element={<Users/>}/>
         <Route path="/Therapies" element={<Therapy/>}/>
         <Route path="*" element= {<Home/>}/>
+        <Route path="/Therapies/<int:pk>" element= {<TherapyDetail/>} />
     </Routes>
  );   
 }
